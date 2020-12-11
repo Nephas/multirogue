aa4ad9f168deda5527c96b4cbc38f4c8267ad56c
Marco Fink <marco.fink@tngtech.com>
2020-12-11 16:54:48 +0100

    figwheel ring handler config

--- rl-client/project.clj
+++ rl-client/project.clj
@@ -1,33 +1,23 @@
 (defproject rlclient "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.async "1.3.610"]
-                 [org.clojure/clojurescript "1.10.520"]
-                 [quil "3.1.0"]]
+  :description "The original figwheel flappybird demo redone for figwheel.main"
 
-  :plugins [[lein-cljsbuild "1.1.7"]
-            [lein-figwheel "0.5.19"]]
-  :hooks [leiningen.cljsbuild]
+  :url "http://rigsomelight.com/2014/05/01/interactive-programming-flappy-bird-clojurescript.html"
 
-  :clean-targets ^{:protect false} ["resources/public/js"]
-  :cljsbuild
-  {:builds [; development build with figwheel hot swap
-            {:id           "development"
-             :source-paths ["src"]
-             :figwheel     {:load-warninged-code false}
-             :compiler     {:main       "rlclient.core"
-                            :output-to  "resources/public/js/main.js"
-                            :output-dir "resources/public/js/development"
-                            :asset-path "js/development"}}
-            ; minified and bundled build for deployment
-            {:id           "optimized"
-             :source-paths ["src"]
-             :compiler
-                           {:main          "rlclient.core"
-                            :output-to     "resources/public/js/main.js"
-                            :output-dir    "resources/public/js/optimized"
-                            :asset-path    "js/optimized"
-                            :optimizations :advanced}}]})
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.312"]
+                 [org.clojure/core.async "0.4.474"]
+                 [quil "3.1.0"]
+                 [cljsjs/react-dom-server "15.3.1-0"]       ;; for sablono
+                 [cljsjs/react-dom "15.3.1-0"]              ;; for sablono
+                 [cljsjs/react "15.3.1-0"]                  ;; for sablono
+                 [sablono "0.7.5"]]
+
+  :resource-paths ["resources" "target"]
+
+  :clean-targets ^{:protect false} ["target/public"]
+
+  :profiles {:dev     {:dependencies [[com.bhauman/figwheel-main "0.1.5"]
+                                      [com.bhauman/rebel-readline-cljs "0.1.4"]]}}
+
+  :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
+            "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "rl" "-r"]})
--- rl-client/src/rlclient/ring.clj
+++ rl-client/src/rlclient/ring.clj
@@ -1,6 +1,7 @@
 (ns rlclient.ring)
 
 (defn handler [req]
+  (println req)
   {:status 404
    :headers {"Content-Type" "text/html"}
    :body "Yep the server failed to find it."})
